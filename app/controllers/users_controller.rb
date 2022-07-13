4bd17082432f21024c9c118344a20d4bd8d4f3c6
Junelle Puzon <nellepuzon@gmail.com>
2022-07-14 05:54:47 +0800

    get and display the latest stock price and total price

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,27 +8,46 @@ class UsersController < ApplicationController
     @client = IEX::Api::Client.new
     @index = [6, 18, 16, 15]
     @stocks = Stock.all
-
+    
     @highest_trade = get_top_trade
-    @highest_shares_bought = @highest_trade[0]
-    @top_trader = User.find(@highest_trade[1])
-    @top_trader_name = get_name(@top_trader)
-
-    @highest_spent =  get_top_spent
-    @amount_spent = @highest_spent[0]
-    @top_spender = User.find(@highest_spent[1])
-    @top_spender_name = get_name(@top_spender)
-
+    if !@highest_trade.nil?
+      @highest_shares_bought = @highest_trade[0]
+      @top_trader = User.find(@highest_trade[1])
+      @top_trader_name = get_name(@top_trader)
+    else
+      @highest_shares_bought = 0
+      @top_trader = User.find_by(role: "admin")
+      @top_trader_name = "None"
+    end
+    
+    @highest_spent = get_top_spent
+    if !@highest_spent.nil?
+      @amount_spent = @highest_spent[0]
+      @top_spender = User.find(@highest_spent[1][0])
+      @top_spender_name = get_name(@top_spender)
+    else
+      @amount_spent = 0
+      @top_spender = User.find_by(role: "admin")
+      @top_spender_name = "None"
+    end
+    
     @richest_trader = @users.each.pluck(:balance, :id).max
-    @highest_balance = @richest_trader[0]
-    @richest_trader = User.find(@richest_trader[1])
-    @richest_trader_name = get_name(@richest_trader)
+    if !@richest_trader.nil?
+      @highest_balance = @richest_trader[0]
+      @richest_trader = User.find(@richest_trader[1])
+      @richest_trader_name = get_name(@richest_trader)
+    else
+      @highest_balance = 0
+      @richest_trader = User.find_by(role: "admin")
+      @richest_trader_name = "None"
+    end
   end
 
   def show
     @user_stocks = @user.user_stocks
     @transactions = @user.transactions
     @stocks = Stock.all
+    @total_spent = get_total_spent || 0
   end
 
   def new
@@ -89,10 +108,14 @@ class UsersController < ApplicationController
     end
 
     def get_top_spent
-      Transaction.group('user_id').group('transaction_type').sum('total_stock_price').invert.max.flatten 
+      Transaction.group('user_id').group('transaction_type').sum('total_stock_price').invert.max 
     end
 
     def get_name(user)
       user.first_name + " " + user.last_name
+    end
+
+    def get_total_spent
+      User.find(params[:id]).transactions.group('transaction_type').sum('total_stock_price')['buy']
     end
 end
