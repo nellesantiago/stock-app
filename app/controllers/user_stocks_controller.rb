bbd64e3f74553b8754d0ed7a581c932a2ea7642b
Junelle Puzon <nellepuzon@gmail.com>
2022-07-13 16:39:51 +0800

    removed stocks and user stocks show view method and routes

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -8,18 +8,13 @@ class UserStocksController < ApplicationController
     @transactions = current_user.transactions
   end
 
-  # GET /user_stocks/1 or /user_stocks/1.json
-  def show
-    @user_stock = current_user.user_stocks.find(params[:id])
-  end
-
   private
 
   def verify_role 
     if current_user.trader?
       return
     else
-      redirect_to users_path, alert: "Function is only for traders"
+      redirect_to users_path, alert: "Function is only for traders!"
     end
   end
 
