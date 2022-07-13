9fa741fc4e8da50292feb3479930b4f8716877ce
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-07-13 18:10:07 +0800

    add redirection to dashboard if a user is signed in

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,6 @@
 class StocksController < ApplicationController
   before_action :verify_status, only: %i[show]
+  before_action :redirect, only: %i[home]
 
   # GET /stocks or /stocks.json
   def index
@@ -16,6 +17,12 @@ class StocksController < ApplicationController
       return
     else
       redirect_to user_stocks_path, alert: "Wait for admin approval"
+    end
+  end
+
+  def redirect 
+    if user_signed_in?
+      redirect_to dashboard_path
     end
   end
 end
